2fa300c23ea7f85d84708690994efc8aac44ccc1
S Sanjith <sanjith.cs20@bmsce.ac.in>
2023-07-19 20:57:09 +0530

    Added more fnctionalities and uninstallation script

--- ideaTweak.cpp
+++ ideaTweak.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <map>
+#include <filesystem>
+
+void getBatteryInformation() {
+    std::string batPath;
+    int cycleCount = 0;
+    int chargeLevel = 0;
+    double designEnergy = 0;
+    double currentFullEnergy = 0;
+    double batteryHealth = 0;
+
+    if (std::filesystem::exists("/sys/class/power_supply/BAT0/")) {
+        batPath = "/sys/class/power_supply/BAT0/";
+    } else if (std::filesystem::exists("/sys/class/power_supply/BAT1/")) {
+        batPath = "/sys/class/power_supply/BAT1/";
+    } else {
+        std::cerr << "Neither BAT0 nor BAT1 exist. Exiting...\n";
+        exit(1);
+    }
+
+    std::ifstream batRead((batPath+"capacity").c_str());
+    batRead >> chargeLevel;
+    batRead.close();
+
+    batRead.open((batPath+"cycle_count").c_str());
+    batRead >> cycleCount;
+    batRead.close();
+
+    batRead.open((batPath+"energy_full_design").c_str());
+    batRead >> designEnergy;
+    designEnergy /= 1000000;
+    batRead.close();
+
+    batRead.open((batPath+"energy_full").c_str());
+    batRead >> currentFullEnergy;
+    currentFullEnergy /= 1000000;
+    batRead.close();
+
+    batRead.open((batPath+"energy_full_design").c_str());
+    batRead >> designEnergy;
+    designEnergy /= 1000000;
+    batRead.close();
+
+    batteryHealth = (currentFullEnergy/designEnergy)*100.0;
+
+    std::cout << "Charge Level: " << chargeLevel << "\n";
+    std::cout << "Cycle Count: " << cycleCount << "\n";
+    std::cout << "Design Full Energy: " << designEnergy << "Wh\n";
+    std::cout << "Current Full Energy: " << currentFullEnergy << "Wh\n";
+    std::cout << "Battery Health: " << batteryHealth << "%\n\n";
+}
+
+void getCurrentStatus() {
+    std::string path = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/";
+    int conservation_mode = 0;
+    int usb_charging = 0;
+    int fn_lock = 0;
+    std::map<int, std::string> state;
+    state[0] = "off";
+    state[1] = "on";
+
+    std::ifstream batRead((path+"conservation_mode").c_str());
+    batRead >> conservation_mode;
+    batRead.close();
+
+    batRead.open((path+"usb_charging").c_str());
+    batRead >> usb_charging;
+    batRead.close();
+
+    batRead.open((path+"fn_lock").c_str());
+    batRead >> fn_lock;
+    batRead.close();    
+
+    std::cout << "Conservation Mode: " << state[conservation_mode] << "\n";
+    std::cout << "USB Charging: " << state[usb_charging] << "\n";
+    std::cout << "Function Lock: " << state[fn_lock] << "\n\n";
+}
 
 int main(int argc, char *argv[]) {
     int opt;
@@ -10,7 +87,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Iterating through the options passed as command line arguments
-    while((opt = getopt(argc, argv, "c:u:f:h")) != -1) {
+    while((opt = getopt(argc, argv, "c:u:f:hbs")) != -1) {
         char value = (optarg) ? *optarg : '\0';
         std::string path = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/";
         std::string feature;
@@ -29,6 +106,15 @@ int main(int argc, char *argv[]) {
                 feature = "fn_lock";
                 break;
             }
+            case 'b':
+                std::cout << "\n---Battery Information---\n";
+                getBatteryInformation();
+                return 0;
+
+            case 's':
+                std::cout << "\n---Current Status---\n";
+                getCurrentStatus();
+                return 0;
 
             // Display Help Message
             case 'h':
@@ -39,7 +125,9 @@ int main(int argc, char *argv[]) {
                 std::cout << "-f, -f <VALUE>   Set option fn_lock to 0 (off) or 1 (on)\n";
                 std::cout << "-u, -u <VALUE>   Set option usb_charging to 0 (off) or 1 (on)\n";
                 std::cout << "-c, -c <VALUE>   Set option conservation_mode to 0 (off) or 1 (on)\n";
-                std::cout << "-h, --help               Display this help message and exit\n";
+                std::cout << "-b,              Display information about the battery\n";
+                std::cout << "-s,              Display current status of Conservation Mode, USB Charging and Function Lock\n";
+                std::cout << "-h,              Display this help message and exit\n";
                 return 0;
             
             default:
